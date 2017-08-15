f8cafbc7023020d2d7c0ebbd1a0babcae4a2613f
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-15 16:46:27 -0500

    Generación de tokens al acceder ok al sistema

--- app/controllers/usuario_controller.php
+++ app/controllers/usuario_controller.php
@@ -8,16 +8,26 @@ class UsuarioController extends Controller
 		$usuario = Flight::request()->query['usuario'];
 		$contrasenia = Flight::request()->query['contrasenia'];
 		$cantidad = Model::factory('Usuario')->where(array('username' => $usuario, 'password' => $contrasenia))->find_array();
+		$rpta = null;
 
 		if(intval($cantidad) >= 1){
-			echo "existe";
+			$token = null;
+			$temp = ORM::for_table('tokens', 'tokens')->where(array('usuario' => $usuario))->find_array();
+			if(intval($temp) == 1){
+				$token = $temp[0]['token'];
+			}else{
+				$token = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(50/strlen($x)) )),1,35);
+				$new_token = ORM::for_table('tokens', 'tokens')->create();
+				$new_token->set('usuario', $usuario);
+				$new_token->set('token', $token);
+				$new_token->save();
+			}
+			$rpta = array("existe"=> 1, "token"=> $token);
 		}else{
-			echo "no existe";
+			$rpta['existe'] = 0;
 		}
 
-		$x = ORM::for_table('tokens', 'tokens')->find_array();
-
-		echo json_encode($x);
+		echo json_encode($rpta);
    	}
 }
 
