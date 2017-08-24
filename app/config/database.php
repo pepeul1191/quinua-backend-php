99006823a475e452a8cea7e14d7f7c49d2e8959b
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-23 19:36:45 -0500

    Agregando parámetros de tokens a db de tokens

--- app/config/database.php
+++ app/config/database.php
@@ -11,9 +11,8 @@ ORM::configure('port', '5432');
 //ORM::configure('sqlite:./db/db_quinua.db');
 //ORM::configure('sqlite:./db/db_tokens.db', null, 'tokens');
 ORM::configure('pgsql:dbname=dc8kfe3kkehphc;host=ec2-23-23-221-255.compute-1.amazonaws.com;', 'tokens');
-ORM::configure('username', 'ekkabwlszqifss');
-ORM::configure('password', '2781d95a789ed3939c85ee2ff3f0552cade3fdbc072d8ab2cd59f48261261ddc');
-ORM::configure('port', '5432');
+ORM::configure('username', 'ekkabwlszqifss', 'tokens');
+ORM::configure('password', '2781d95a789ed3939c85ee2ff3f0552cade3fdbc072d8ab2cd59f48261261ddc', 'tokens');
 
 ORM::configure('return_result_sets', true);
 ORM::configure('error_mode', PDO::ERRMODE_WARNING);
