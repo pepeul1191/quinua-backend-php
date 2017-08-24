2f26750946b4f0c683c2eb344cbe30843cd1b5f7
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-23 19:55:53 -0500

    Parametro de la db username por user

--- app/config/database.php
+++ app/config/database.php
@@ -3,8 +3,8 @@
 require_once 'app/vendor/j4mie/idiorm/idiorm.php';
 require_once 'app/vendor/j4mie/paris/paris.php';
 
-ORM::configure('pgsql:dbname=quinua;host=168.121.220.36;username=postgres;password=ulima');
-ORM::configure('pgsql:dbname=dc8kfe3kkehphc;host=ec2-23-23-221-255.compute-1.amazonaws.com;username=ekkabwlszqifss;password:2781d95a789ed3939c85ee2ff3f0552cade3fdbc072d8ab2cd59f48261261ddc', 'tokens');
+ORM::configure('pgsql:dbname=quinua;host=168.121.220.36;user=postgres;password=ulima');
+ORM::configure('pgsql:dbname=dc8kfe3kkehphc;host=ec2-23-23-221-255.compute-1.amazonaws.com;user=ekkabwlszqifss;password:2781d95a789ed3939c85ee2ff3f0552cade3fdbc072d8ab2cd59f48261261ddc', 'tokens');
 
 ORM::configure('return_result_sets', true);
 ORM::configure('error_mode', PDO::ERRMODE_WARNING);
