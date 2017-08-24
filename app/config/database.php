b660c0a91359253f7c030ba9b6da15e91aa18221
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-23 19:06:07 -0500

    Quitando archivos innecesarios de ruby

--- app/config/database.php
+++ app/config/database.php
@@ -3,13 +3,12 @@
 require_once 'app/vendor/j4mie/idiorm/idiorm.php';
 require_once 'app/vendor/j4mie/paris/paris.php';
 
-/*
 ORM::configure('pgsql:dbname=quinua;host=168.121.220.36;');
 ORM::configure('username', 'postgres');
 ORM::configure('password', 'ulima');
 ORM::configure('port', '5432');
-*/
-ORM::configure('sqlite:./db/db_quinua.db');
+
+//ORM::configure('sqlite:./db/db_quinua.db');
 
 ORM::configure('sqlite:./db/db_tokens.db', null, 'tokens');
 ORM::configure('return_result_sets', true);
