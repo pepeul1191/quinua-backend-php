866171cd0d80a7b1e1e6e9f9a5d07e9aa813ec14
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-23 19:52:58 -0500

    Cambiando la configuración de postgres para la db remota ulima

--- app/config/database.php
+++ app/config/database.php
@@ -3,11 +3,7 @@
 require_once 'app/vendor/j4mie/idiorm/idiorm.php';
 require_once 'app/vendor/j4mie/paris/paris.php';
 
-ORM::configure('pgsql:dbname=quinua;host=168.121.220.36;');
-ORM::configure('username', 'postgres');
-ORM::configure('password', 'ulima');
-ORM::configure('port', '5432');
-
+ORM::configure('pgsql:dbname=quinua;host=168.121.220.36;username:postgres;password:ulima');
 ORM::configure('pgsql:dbname=dc8kfe3kkehphc;host=ec2-23-23-221-255.compute-1.amazonaws.com;username=ekkabwlszqifss;password:2781d95a789ed3939c85ee2ff3f0552cade3fdbc072d8ab2cd59f48261261ddc', 'tokens');
 
 ORM::configure('return_result_sets', true);
