405428ef4d09a71d35ddb906fd9b9466d8bce56a
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-23 19:48:58 -0500

    Quitando todo lo que hay de sqlite

--- app/config/database.php
+++ app/config/database.php
@@ -8,13 +8,10 @@ ORM::configure('username', 'postgres');
 ORM::configure('password', 'ulima');
 ORM::configure('port', '5432');
 
-//ORM::configure('sqlite:./db/db_quinua.db');
-//ORM::configure('sqlite:./db/db_tokens.db', null, 'tokens');
 ORM::configure('pgsql:dbname=dc8kfe3kkehphc;host=ec2-23-23-221-255.compute-1.amazonaws.com;username=ekkabwlszqifss;password:2781d95a789ed3939c85ee2ff3f0552cade3fdbc072d8ab2cd59f48261261ddc', 'tokens');
 
 ORM::configure('return_result_sets', true);
 ORM::configure('error_mode', PDO::ERRMODE_WARNING);
-//ORM::configure('sqlite:./db/db_tokens.db', null, 'tokens');
 ORM::configure('return_result_sets', true);
 ORM::configure('error_mode', PDO::ERRMODE_WARNING);
 
